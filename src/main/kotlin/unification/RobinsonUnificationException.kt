d02b8c8e199bfa1b586b18e1ea655fa87a2db976
Max Kørner Andersen <maxkandersen@gmail.com>
2026-03-06 14:41:04 +0100

    Refactor: Removed unnecessary "val"

--- src/main/kotlin/unification/RobinsonUnificationException.kt
+++ src/main/kotlin/unification/RobinsonUnificationException.kt
@@ -2,4 +2,4 @@ package dk.maxkandersen.unification
 
 import dk.maxkandersen.type.Type
 
-class RobinsonUnificationException(val s: Type, val t: Type) : RuntimeException("Unification error: Could not unify $s and $t")
+class RobinsonUnificationException(s: Type, t: Type) : RuntimeException("Unification error: Could not unify $s and $t")
